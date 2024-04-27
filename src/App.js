19a02650dec94f21654136b041dfdaad9ad4f9db
Pavel <pawelv9@gmail.com>
2024-04-27 17:25:18 +0300

    added logic for BookCreate

--- src/App.js
+++ src/App.js
@@ -1,7 +1,16 @@
+import { useState } from 'react';
+import BookCreate from './components/BookCreate';
+
 function App() {
+  const [books, setBooks] = useState()
+
+  const createBook = title => {
+    console.log('Needed to add book with:', title)
+  }
+
   return (
     <div>
-
+      <BookCreate onCreate={createBook}/>
     </div>
   );
 }
