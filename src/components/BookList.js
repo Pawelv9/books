496154b19ed7ae1728a3f375d3557475f9b4ea61
Pavel <pawelv9@gmail.com>
2024-04-29 17:04:28 +0300

    displaying the list of books

--- src/components/BookList.js
+++ src/components/BookList.js
@@ -1,8 +1,12 @@
+import BookShow from './BookShow'
 
+function BookList({ books }) {
+  const renderedBooks = books.map((book) => {
+    return <BookShow key={books.id} book={book}/>
+  })
 
-function BookList() {
   return (
-    <div>BookList</div>
+    <div className='book-list'>{renderedBooks}</div>
   )
 }
 
